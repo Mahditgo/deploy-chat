1b73a1b1ff46f95dde8dd85a0d8f2a20fbca6936
Mahdi <mahdi90056@gmail.com>
2025-04-03 20:49:43 +0330

    dfhgdfh

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,13 @@
-import axios from "axios";
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+//   withCredentials: true,
+// });
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  withCredentials: true,
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://localhost:5001/api" 
+    : "http://89.42.199.75:5001/api",  // اینجا `baseURL` رو مشخص کردیم
+  withCredentials: true, // برای ارسال کوکی‌ها
 });
